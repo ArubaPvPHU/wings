0c69e5a1f6f5353a168bd5a3f8bc324c51cc1539
Figura Szilárd <figuraszilard@gmail.com>
2024-11-18 21:11:54 +0100

    Test2

--- discord/discordHook.go
+++ discord/discordHook.go
@@ -165,5 +165,5 @@ func GetServerByUUID(uuid string) string {
 }
 
 func CheckEnvVars() {
-	fmt.Printf("WEBHOOK_URL in discord package: %s\n", os.Getenv("WEBHOOK_URL"))
+	log.Println("WEBHOOK_URL in discord package: %s\n", os.Getenv("WEBHOOK_URL"))
 }
